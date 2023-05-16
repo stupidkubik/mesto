5e5718803969b165dca23ad7a1fdca047c09d4ee
Evgenii Rubin <kubikus2009@yandex.ru>
2023-05-16 17:33:35 +0300

    feat: добавить файлы вебпака

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-console.log("Hello, World!");
+(()=>{var n=[2,3,5].map((function(n){return 2*n}));console.log(n)})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('Hello, World!') 
+const numbers = [2, 3, 5];
+
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers);
